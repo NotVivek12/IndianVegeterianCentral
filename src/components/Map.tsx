fd0a2c6590cb3ffe4af0d4070af31dc6ead7e409
notvivek12 <vivekjoshi82015@gmail.com>
2025-09-14 20:33:27 +0530

    fixed some library issues

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,15 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Popup } from 'leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+
+// Fix Leaflet's default icon path issues
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+});
 
 interface MapProps {
   center: [number, number];
